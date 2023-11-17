36d1c6a1e899f7ec83139e6c8bcd71e946a2b8b5
ThiagoFAm <thiagoamaraldv@gmail.com>
2023-11-16 21:06:34 -0400

    Reorganizacao

---
+++ DIO/Python/00 - Fundamentos/Boas Práticas/Códigos/Comentarios.py
@@ -0,0 +1,11 @@
+name = str("thiago")
+
+def print_hi_name(name):
+    """
+    isso é uma docstring, usada para também fazer comentários
+    :param name: Nome predefinido antes da função
+    :return: retorna via print.
+    """
+    print(f"Hi {name}")
+    
+    return   
--- DIO/Python/Fundamentos/Boas Práticas/Códigos/Tab.py
+++
@@ -1,4 +0,0 @@
-#A tecla tab além de identar, tambem serve como autocomplete
-
-#Ctrl + alt + l -> organiza a identação do codigo
-
